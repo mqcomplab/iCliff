81728035167dac4941ed51da03f7f65eb9c01761
klopezperez <klopezperez@ufl.edu>
2025-03-05 19:55:47 -0500

    updated analysis

--- icliff_analysis.py
+++ icliff_analysis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from iCliff.iCliff import calculate_iCliff
+from iCliff.iCliff import calculate_iCliff, ts_sali_matrix
 import pandas as pd
 import glob
 
@@ -16,9 +16,6 @@ for file in glob.glob('/blue/rmirandaquintana/klopezperez/ecliffs/fps/CHEMBL*.pk
     props = data['prop']
     fps = np.array(data[fp_type])
 
-    # Read if its an AC or not
-    ac = np.array(data['cliff']) 
-
     del data
 
     # Convert and normalize the properties
@@ -28,20 +25,38 @@ for file in glob.glob('/blue/rmirandaquintana/klopezperez/ecliffs/fps/CHEMBL*.pk
     # Normalize the properties
     props = (props - np.min(props))/(np.max(props) - np.min(props))
 
+    # Load the similarity matrix
+    sim_matrix = np.load(f'/blue/rmirandaquintana/klopezperez/ecliffs/pair_matrices/JT/{fp_type}/{name}_{fp_type}_JT.npy')
+
+    # Load the property difference matrix
+    prop_diff = np.load(f'/blue/rmirandaquintana/klopezperez/ecliffs/pair_matrices/prop_diffs/{name}.npy')
+
+    # Calculate the ts_sali matrix
+    ts_sali = ts_sali_matrix(prop_diff, sim_matrix, 3)
+
+    # Find the positions where you find the top 10% of the ts_sali matrix, think that this is a 2D matrix
+    top_10 = np.percentile(ts_sali, 90)
+
+    # Find the positions where you find the top 10% of the ts_sali matrix
+    top_10_positions = np.argwhere(ts_sali > top_10)
+
+    # Get the unique indexes
+    unique_positions = np.unique(top_10_positions.flatten())
+
     # Calculate the iCliff value
     iCliff = calculate_iCliff(fps, props)
 
-    # Select only the fingeprints that are not AC
-    fps_no_AC = fps[ac == 0]
-    fps_AC = fps[ac == 1]
+    # Select only the fingeprints that are not AC (on the unique positions)
+    fps_AC = fps[unique_positions]
+    fps_no_AC = np.delete(fps, unique_positions, axis=0)
 
-    # Select only the properties that are not AC
-    props_no_AC = props[ac == 0]
-    props_AC = props[ac == 1]    
+    # Select only the properties that are not AC (on the unique positions)
+    props_AC = props[unique_positions]
+    props_no_AC = np.delete(props, unique_positions)
 
     # Calculate the iCliff value
-    iCliff_no_AC = calculate_iCliff(fps, props)
-    iCliff_AC = calculate_iCliff(fps, props)
+    iCliff_no_AC = calculate_iCliff(fps_no_AC, props_no_AC)
+    iCliff_AC = calculate_iCliff(fps_AC, props_AC)
 
     entry = [name, iCliff, iCliff_no_AC, iCliff_AC, len(fps), len(fps_no_AC), len(fps_AC)]
     print(entry[:4])
