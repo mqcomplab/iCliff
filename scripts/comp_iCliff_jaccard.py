6a6edb2ea6863763be8f478208154254fd0fef00
klopezperez <klopezperez@ufl.edu>
2025-03-08 19:43:32 -0500

    jaccard script

---
+++ scripts/comp_iCliff_jaccard.py
@@ -0,0 +1,24 @@
+import numpy as np
+from iCliff.iCliff import calculate_comp_iCliff, sali_analysis
+import glob
+
+fp_type = 'ECFP4'
+kendalls = []
+for file in glob.glob('data/props/*.npy'):
+    # Get the name
+    name = file.split('/')[-1].split('.')[0]
+
+    # Read the props
+    props = np.load(file, mmap_mode='r')
+
+    # Read the fingerprints
+    fps = np.load(f'data/fps/{fp_type}/{name}.npy', mmap_mode='r')
+
+    # Read the ts_sali matrix
+    ts_sali = np.load(f'data/ts_sali_matrix/{fp_type}/{name}.npy', mmap_mode='r')
+
+    # Calculate the iCliff values
+    iCliff = calculate_comp_iCliff(fps, props)
+
+    # Do the sali analysis
+    df = sali_analysis(iCliff, ts_sali, f'results/jaccard/{name}.csv')
