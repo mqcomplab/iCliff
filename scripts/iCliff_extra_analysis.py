8efc736a7f1b202e2e66133fcd0ce9e1f3fbffd3
klopezperez <klopezperez@ufl.edu>
2025-04-22 15:16:02 -0400

    updated extra analysis

--- scripts/iCliff_extra_analysis.py
+++ scripts/iCliff_extra_analysis.py
@@ -1,5 +1,5 @@
 import numpy as np
-from iCliff.iCliff import calculate_comp_iCliff, squared_prop_diff_matrix, tanimoto_similarity_matrix, ts_sali_matrix, tanimoto_similarity
+from iCliff.iCliff import calculate_comp_iCliff, pair_prop_diff, mol_set_tanimoto, ts_sali_matrix
 import pandas as pd
 import glob
 
@@ -8,63 +8,87 @@ for file in glob.glob('data/props/*.npy'):
     # Get the name
     name = file.split('/')[-1].split('.')[0]
 
-    # Read the props data
-    props = np.load(file, mmap_mode='r')
-
     # Read the fingerprints data
     fps = np.load(f'data/fps/{fp_type}/{name}.npy', mmap_mode='r')
 
     # Read the smiles data
     smiles = pd.read_csv(f'data/smiles/{name}.csv')
+    props = smiles['exp_mean [nM]'].values
     smiles = smiles['smiles'].values
+    props = props/10**9
+    props = -np.log10(props)
+
+    # Read the ts_sali_matrix data
+    ts_sali = np.load(f'data/ts_sali_matrix/{fp_type}/{name}.npy', mmap_mode='r')
 
     # Calculate the comp_iCliff values
     comp_iCliff = calculate_comp_iCliff(fps, props)
 
-    # Sort the iCliff values, get the 10% with lowest comp iCliff values
+    # Sort the iCliff values, get the 1% with lowest comp iCliff values
     idx = np.argsort(comp_iCliff)
-    idx = idx[int(len(idx)*0.1):]
+    idx = idx[int(len(idx)*0.01):]
 
-    # Get the properties and fingerprints of the 10% with lowest comp iCliff values
-    props_ = props[idx]
-    fps_ = fps[idx]
-    smiles_ = smiles[idx]
+    # Get the ts_sali values for the selected indices
+    ts_sali = ts_sali[idx]
 
-    # Calculate the squared property difference matrix
-    prop_diff_matrix = squared_prop_diff_matrix(props_)
+    smiles_1 = []
+    smiles_2 = []
+    props_1 = []
+    props_2 = []
+    sim = []
+    ts_salis = []
+    # Save the instances where the TS_SALI values are the in the top 10 for each compound in the rows
+    for i in range(len(idx)):
+        # Get the top 10% of the ts_sali values for each compound
+        top = np.percentile(ts_sali[i], 90)
 
-    # Calculate the Tanimoto similarity matrix
-    sim_matrix = tanimoto_similarity_matrix(fps_)
+        # Get the indices of the top 10% values
+        top_indices = np.argwhere(ts_sali[i] > top)
+        top_indices = top_indices[:, 0]
 
-    # Calculate the ts_sali matrix
-    ts_sali = ts_sali_matrix(prop_diff_matrix, sim_matrix, term=3)
+        # Get the ts_sali values of the problematic compound and their 10% according to TSSALI
+        ts_sali_prob = ts_sali[i][top_indices]
 
-    # Find the positions where you find the 10% highest values of the ts_sali matrix
-    top = np.percentile(ts_sali, 90)
-    top_positions = np.argwhere(ts_sali > top)
-    
-    entries = []
-    # Create the data entries
-    for i, j in top_positions:
-        # Get the smiles of the two compounds
-        smiles_1 = smiles_[i]
-        smiles_2 = smiles_[j]
+        # Get the smiles of the problematic compound and their 10% according to TSSALI
+        smiles_prob = smiles[idx[i]]
+        smiles_top = smiles[idx[top_indices]]
 
-        # Get the properties of the two compounds
-        props_1 = props_[i]
-        props_2 = props_[j]
+        # Replicate the problematic compound smiles as many times as the top 10% according to TSSALI
+        smiles_prob = np.array([smiles_prob]*len(smiles_top))
 
-        # Get the similarity of the two compounds
-        sim = tanimoto_similarity(fps_[i], fps_[j])
+        # Get the properties of the problematic compound and their 10% according to TSSALI
+        props_prob = props[idx[i]]
+        props_top = props[idx[top_indices]]
 
-        # Get the ts_sali values
-        iCliff_pair = ts_sali[i, j]
+        # Get the fps of the problematic compound and their 10% according to TSSALI
+        fps_prob = fps[idx[i]]
+        fps_top = fps[idx[top_indices]]
 
-        # Append the data
-        entries.append([idx[i], idx[j], smiles_1, smiles_2, props_1, props_2, sim, iCliff_pair])
+        # Get the similarity of the problematic compound and their 10% according to TSSALI
+        sim_prob = mol_set_tanimoto(fps_prob, fps_top)
 
-    # Save the results
-    df = pd.DataFrame(entries, columns = ['idx_1', 'idx_2', 'smiles_1', 'smiles_2', 'props_1', 'props_2', 'similarity', 'iCliff'])
+        # Append the values to the lists
+        smiles_1.append(smiles_prob)
+        smiles_2.append(smiles_top)
+        props_1.append(props_prob)
+        props_2.append(props_top)
+        sim.append(sim_prob)
+        ts_salis.append(ts_sali_prob)
+
+    # Save the values to a dataframe
+    df = pd.DataFrame({
+        'smiles_1': smiles_1,
+        'smiles_2': smiles_2,
+        'props_1': props_1,
+        'props_2': props_2,
+        'sim': sim,
+        'ts_sali': ts_salis
+    })
+
+    # Save the dataframe to a csv file
     df.to_csv(f'results/pair_identification/{name}_iCliff.csv', index=False)
 
-    print(f'Finished {name}')
+    # Print finished
+    print(f'Finished {name} iCliff analysis')
+
+        
